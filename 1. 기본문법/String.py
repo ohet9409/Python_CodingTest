174ff39c6d629b5d33f3ad2b7c94cb912d47393d
oet <54170438+ohet9409@users.noreply.github.com>
2022-01-25 19:57:56 +0900

    문법 -> 자료형 -> 문자열 자료형 공부

---
+++ 1. 기본문법/String.py
@@ -0,0 +1,20 @@
+data = 'Hello World'
+print(data)
+
+data = "Don't ypu Know \"Python\"?"
+print(data)
+
+print("-------------------------")
+
+a = "Hello"
+b = "World"
+print(a + " " +b)
+print("-------------------------")
+
+a = "String"
+print(a * 3)
+
+print("-------------------------")
+
+a = "ABCDEF"
+print(a[2:4])
