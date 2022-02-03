6ce426972380ce60220053afdea08e941056376d
oet <54170438+ohet9409@users.noreply.github.com>
2022-02-03 19:39:45 +0900

    문법 -> 입출력 공부

---
+++ 1. 기본문법/print.py
@@ -0,0 +1,48 @@
+# 데이터의 개수 입력
+#n = int(input())
+# 각 데이터를 공백으로 구분하여 입력
+#data = list(map(int, input().split()))
+
+#data.sort(reverse=True)
+#print(data)
+
+print('--------------------------')
+
+#n, m, k를 공백으로 구분하여 입력
+#n, m, k = map(int, input().split())
+#print(n,m,k)
+
+print('--------------------------')
+
+import sys
+
+# 문자열 입력받기
+data = sys.stdin.readline().rstrip()
+print(data)
+
+print('--------------------------')
+
+# 출력할 변수들
+a = 1
+b = 2
+print(a, b)
+
+print('--------------------------')
+
+# 출력할 변수들
+a = 1
+b = 2
+print(a)
+print(b)
+print('--------------------------')
+
+answer = 7
+print("정답은 " + str(answer) + "입니다.")
+print('--------------------------')
+
+answer = 7
+print("정답은 ", answer, "입니다.")
+print('--------------------------')
+answer = 7
+print(f"정답은 {answer}입니다.")
+
