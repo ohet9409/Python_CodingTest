886d6d0dc0002e236614962e3c302069bc21e71f
oet <54170438+ohet9409@users.noreply.github.com>
2022-02-06 20:41:27 +0900

    itertools 공부

---
+++ 1. 기본문법/itertools.py
@@ -0,0 +1,31 @@
+from itertools import permutations
+data = ['A', 'B', 'C'] # 데이터 준비
+result = list(permutations(data,3)) # 모든 순열 구하기
+print(result)
+
+result = list(permutations(data,2)) # 모든 순열 구하기
+print(result)
+
+print("---------------------------------------------")
+from itertools import combinations
+data = ['A', 'B', 'C'] # 데이터 준비
+result = list(combinations(data,2)) # 2개를 뽑는 모든 조합 구하기
+print(result)
+result = list(combinations(data,3)) # 3개를 뽑는 모든 조합 구하기
+print(result)
+print("---------------------------------------------")
+
+from itertools import product
+data = ['A', 'B', 'C'] # 데이터 준비
+result = list(product(data, repeat=2)) # 2개를 뽑는 모든 순열 구하기(중복허용)
+print(result)
+result = list(product(data, repeat=3)) # 3개를 뽑는 모든 순열 구하기(중복허용)
+print(result)
+
+print('---------------------------------------------')
+from itertools import combinations_with_replacement
+data = ['A', 'B', 'C'] # 데이터 준비
+result = list(combinations_with_replacement(data,2)) # 2개를 뽑는 모든 조합 구하기(중복 허용)
+print(result)
+result = list(combinations_with_replacement(data,3)) # 3개를 뽑는 모든 조합 구하기(중복 허용)
+print(result)
