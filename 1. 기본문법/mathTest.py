a70fa5ec1ed35c59015f0b34e321ee0acdad87b7
oet <54170438+ohet9409@users.noreply.github.com>
2022-02-10 10:18:35 +0900

    math 공부

---
+++ 1. 기본문법/mathTest.py
@@ -0,0 +1,16 @@
+import math
+
+print(math.factorial(5)) # 5 팩토리얼을 출력
+
+print('-------------------------------')
+import math
+print(math.sqrt(7)) # 7의 제곱근을 출력
+
+print('-------------------------------')
+import math
+print(math.gcd(21,14)) # 21과 14의 최대 공약수 출력
+
+print('-------------------------------')
+import math
+print(math.pi) # 파이(pi) 출력
+print(math.e) # 자연상수 e 출력
