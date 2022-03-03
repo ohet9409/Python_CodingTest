849eb55adbb2012721a9723b0450fe5e1ee3494c
oet <54170438+ohet9409@users.noreply.github.com>
2022-03-03 19:25:06 +0900

    선택 정렬과 기본 정렬 라이브러리의 수행 시간 비교

--- 1. 기본문법/exam1/ex3.py
+++ 1. 기본문법/exam1/ex3.py
@@ -7,6 +7,7 @@ array = []
 for _ in range(10000):
     array.append(randint(1, 100)) # 1 부터 100 사이의 랜덤한 정숴
 
+print("배열 초기화: ", array)
 # 선택 정렬 프로그램 성능 측정
 start_time = time.time()
 
@@ -19,18 +20,20 @@ for i in range(len(array)):
     array[i], array[min_index] = array[min_index], array[i] # 스와프
 
 end_time = time.time() # 측정 종료
+print("정렬 확인: ", array)
 print("선택 정렬 성능 측정: ", end_time - start_time) # 수행 시간 출력
 
 # 배열을 다시 무작위 데이터로 초기화
-array = []
+array2 = []
 for _ in range(10000):
-    array.append(randint(1,100)) # 1부터 100 사이의 랜덤한 정수
-
+    array2.append(randint(1,100)) # 1부터 100 사이의 랜덤한 정수
+print("배열 초기화: ", array2)
 # 기본 정렬 라이브러리 성능 측정
 start_time = time.time()
 
 # 기본 정렬 라이브러리 사용
-array.sort()
+array2.sort()
 
 end_time = time.time() # 측정 종료
+print("정렬 확인: ", array2)
 print("기본 정렬 라이브러리 성능 측정:" , end_time - start_time) # 수행 시간 측정
